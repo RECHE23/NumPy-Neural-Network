d4913d1e678c56598376150ba4ed2fafd934eb0d
René Chenard <rene.chenard.1@ulaval.ca>
2023-08-21 16:35:22 -0400

    Implementation of __call__, __str__ and __repr__ for Layer.

--- NeuralNetwork/layers/layer.py
+++ NeuralNetwork/layers/layer.py
@@ -1,13 +1,26 @@
 from abc import abstractmethod
 from NeuralNetwork.tools import trace
+import itertools
 
 
 class Layer:
+    id_iter = itertools.count()
+
     def __init__(self):
+        self.id = next(self.id_iter)
         self.input = None
         self.output = None
         self.retrograde = None
         self.upstream_gradients = None
+
+    def __call__(self, *args, **kwargs):
+        return self.forward_propagation(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(Layer #{self.id})"
 
     @trace()
     def forward_propagation(self, input_data):
