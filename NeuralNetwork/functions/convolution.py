57a7a6f92dff6189faef0dd251f3a5be275c9b81
René Chenard <rene.chenard.1@ulaval.ca>
2023-08-22 12:17:39 -0400

    Better implementation of correlate2d and convolve2d.

--- NeuralNetwork/functions/convolution.py
+++ NeuralNetwork/functions/convolution.py
@@ -3,29 +3,39 @@ from concurrent.futures import ThreadPoolExecutor
 import numpy as np
 
 
-def correlate2d(input_array, kernel, mode="full"):
+def correlate2d(input_array, kernel, mode="full", boundary="constant", fillvalue=0):
     """
     Equivalent to scipy.signal.correlate2d.
     """
-    if mode == "full":
-        h_padding = kernel.shape[0] - 1  # Horizontal padding
-        v_padding = kernel.shape[1] - 1  # Vertical padding
-        input_array = np.pad(input_array, [(h_padding, h_padding), (v_padding, v_padding)], mode='constant')
+    padded_array = padding(input_array, kernel, mode, boundary, fillvalue)
+    sliding_view = np.lib.stride_tricks.sliding_window_view(padded_array, kernel.shape)
 
-    return np.einsum('ijkl,kl->ij', np.lib.stride_tricks.sliding_window_view(input_array, kernel.shape), kernel)
+    return np.einsum('ijkl,kl->ij', sliding_view, kernel)
 
 
-def convolve2d(input_array, kernel, mode="full"):
+def convolve2d(input_array, kernel, mode="full", boundary="constant", fillvalue=0):
     """
     Equivalent to scipy.signal.convolve2d.
     """
-    if mode == "full":
-        h_padding = kernel.shape[0] - 1  # Horizontal padding
-        v_padding = kernel.shape[1] - 1  # Vertical padding
-        input_array = np.pad(input_array, [(h_padding, h_padding), (v_padding, v_padding)], mode='constant')
+    padded_array = padding(input_array, kernel, mode, boundary, fillvalue, pad_after=True)
+    sliding_view = np.lib.stride_tricks.sliding_window_view(padded_array, kernel.shape)
 
-    kernel = np.flip(kernel)
-    return np.einsum('ijkl,kl->ij', np.lib.stride_tricks.sliding_window_view(input_array, kernel.shape), kernel)
+    return np.einsum('ijkl,kl->ij', sliding_view, np.flip(kernel))
+
+
+def padding(array, kernel, mode, boundary="constant", fillvalue=0, pad_after=False):
+    if mode != "valid":
+        if mode == "full":
+            padding_sizes = [(s - 1, s - 1) for s in kernel.shape]
+        elif mode == "same":
+            padding_sizes = [((s - 1) // 2, s - 1 - (s - 1) // 2) for s in kernel.shape]
+            if pad_after:
+                padding_sizes = [p[::-1] for p in padding_sizes]
+        else:
+            raise NotImplementedError
+
+        array = np.pad(array, padding_sizes, mode=boundary, constant_values=fillvalue)
+    return array
 
 
 def parallel_iterator(function, *iterables):
